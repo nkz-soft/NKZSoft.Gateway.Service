20359ff61a8854807176c5145206325aa668b6aa
nkz <5499916+nkz-soft@users.noreply.github.com>
2025-07-21 20:30:43 +0300

    Upgrade project to .NET 9.0, update dependencies, and enhance Keycloak integration in Docker setup. Revise README for improved clarity on architecture and setup instructions.

--- src/NKZSoft.Gateway.API/GlobalUsings.cs
+++ src/NKZSoft.Gateway.API/GlobalUsings.cs
@@ -4,4 +4,5 @@ global using System.Security.Claims;
 global using Microsoft.AspNetCore.Authentication;
 global using Microsoft.AspNetCore.Authentication.Cookies;
 global using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+global using Microsoft.IdentityModel.Protocols;
 global using Microsoft.IdentityModel.Protocols.OpenIdConnect;
